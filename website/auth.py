447badebebe5fcd581668c3c5961c1a1a1018ca0
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2021-04-22 18:36:10 -0500

    Se creo la logica de singup y se agrego los mensajes flash

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,37 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
-auth = Blueprint('auth',__name__,static_folder='static', template_folder='templates')
+auth = Blueprint("auth", __name__, static_folder="static", template_folder="templates")
 
-@auth.route('/login')
+
+@auth.route("/login", methods=["GET", "POST"])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
 
-@auth.route('/sign-up')
+@auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+        email = request.form.get("email")
+        first_name = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if len(email) < 4:
+            flash("Email must be greater than 3 characters.", category="error")
+        elif len(first_name) < 2:
+            flash("First name must be greater than 1 character.", category="error")
+        elif password1 != password2:
+            flash("Passwords don't match.", category="error")
+        elif len(password1) < 7:
+            flash("Password must be at least 7 characters.", category="error")
+        else:
+            flash("Account created!", category="success")
+
     return render_template("sign-up.html")
 
 
-@auth.route('/logout')
+@auth.route("/logout")
 def logout():
-    return '<h1> Sing Up </h1>'
+    return "<h1> Sing Up </h1>"
