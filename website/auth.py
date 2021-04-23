6df915d1c4ded7d63c3e1cdaf13441c75f3d104a
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2021-04-22 21:34:11 -0500

    Arreglos menores

--- website/auth.py
+++ website/auth.py
@@ -50,8 +50,9 @@ def sign_up():
                 password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            user = User.query.filter_by(email=email).first()
             flash("Account created!", category="success")
+            login_user(user, remember=True)
             return redirect(url_for('views.home'))
 
     return render_template("sign-up.html",user=current_user)
