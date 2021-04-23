36d599192c882848cadc1ef0ee33753d829abc64
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2021-04-22 20:54:03 -0500

    Verifica si el usuario esta o no registrado

--- website/auth.py
+++ website/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
-
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint("auth", __name__, static_folder="static", template_folder="templates")
 
@@ -17,6 +17,7 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
@@ -49,6 +50,7 @@ def sign_up():
                 password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash("Account created!", category="success")
             return redirect(url_for('views.home'))
 
@@ -56,5 +58,7 @@ def sign_up():
 
 
 @auth.route("/logout")
+@login_required
 def logout():
-    return "<h1> Sing Up </h1>"
+    logout_user()
+    return redirect(url_for('auth.login'))
